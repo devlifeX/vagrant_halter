6dd695a80ccc6e8e422398edc295c2382a7ed350
Dariush vesal <dariush.vesal@gmail.com>
2016-10-14 05:43:17 +0330

    Initial commit 23/07/1395 saat 5 o 43 bamdad :)

---
+++ vagrant_halter.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#color
+BS="\033[1m";
+BF="\033[0m";
+RED="\e[31m";
+GREEN="\e[32m";
+
+print () {
+  patterns=();
+  patterns+=("\(%bs\)");
+  patterns+=("\(%bf\)");
+
+  replace=();
+  replace+=($BS);
+  replace+=($BF);
+
+  message=$1;
+  counter=0;
+  for pattern in ${patterns[@]}; do
+    close=${replace[$counter]}$2;
+    message=${message//$pattern/$close};
+    counter=$[$counter+1];
+  done
+
+  if [[ $2 ]]; then
+    message="$2$message$BF";
+  fi
+
+  echo -e $message;
+}
+
+VAGRANT=`vagrant global-status | grep running`
+
+vagrant_halter() {
+  IFS=$'\n' read -d '' -r -a  vagrantArray <<< "$VAGRANT"
+
+  for item in "${vagrantArray[@]}"; do
+    IFS=' ' read -d '' -r -a  itemArray <<< "$item"
+    cd "${itemArray[4]}"
+    print "Halting (%bs)${itemArray[4]}(%bf)." $GREEN;
+    vagrant halt "${itemArray[0]}"
+  done
+}
+
+if [ "$VAGRANT" == "" ]; then
+ print "there is no running VM." $GREEN;
+ print "bye, have good day ;)" $GREEN;
+ exit
+else
+  read -p "Are you sure? [y/n]" -n 1 -r
+  echo   
+  if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+    vagrant_halter "$VAGRANT"
+  else
+    print "You abort operation!" $RED;
+    exit
+  fi
+fi
