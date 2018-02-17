17b45747a633d83eed28db5669437ebf793f9cf3
Dariush <devlifeX@users.noreply.github.com>
2018-02-17 17:06:45 +0330

    Update installer_vagrant_halter.sh
    Fix add to ~/.bashrc

--- installer_vagrant_halter.sh
+++ installer_vagrant_halter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo cp vagrant_halter.sh /bin
 sudo chmod 777 /bin/vagrant_halter.sh
-sudo echo "alias vagrant_halter='bash vagrant_halter.sh' >> ~/.bashrc
+sudo echo "alias vagrant_halter='bash vagrant_halter.sh'" >> ~/.bashrc
 sudo echo "alias halter='bash vagrant_halter.sh'" >> ~/.bashrc
 sudo echo "alias vh='bash vagrant_halter.sh'" >> ~/.bashrc
 source ~/.bashrc
